bfe34b5bdf7b26c477c5097cebcbaa3476d9af29
hepha02 <haka.gpt@gmail.com>
2026-06-29 19:44:35 +0900

    Add latest version redirect helper

---
+++ latest-redirect.js
@@ -0,0 +1,11 @@
+const LATEST_VERSION = "auth-resend-strong-2";
+const params = new URLSearchParams(location.search);
+const currentVersion = params.get("v") || "";
+const oldVersion = currentVersion && currentVersion !== LATEST_VERSION;
+
+if (oldVersion && !sessionStorage.getItem(`haka_latest_redirect_${LATEST_VERSION}`)) {
+  sessionStorage.setItem(`haka_latest_redirect_${LATEST_VERSION}`, "1");
+  const next = new URL(location.href);
+  next.searchParams.set("v", LATEST_VERSION);
+  location.replace(next.toString());
+}
