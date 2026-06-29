82f79a6ae81442619bee3fa2cdb293cf7e5abf5c
hepha02 <haka.gpt@gmail.com>
2026-06-29 20:07:53 +0900

    Point latest redirect to auth resend strong 4

--- latest-redirect.js
+++ latest-redirect.js
@@ -1,4 +1,4 @@
-const LATEST_VERSION = "auth-resend-strong-3";
+const LATEST_VERSION = "auth-resend-strong-4";
 const params = new URLSearchParams(location.search);
 const currentVersion = params.get("v") || "";
 const oldVersion = currentVersion && currentVersion !== LATEST_VERSION;
