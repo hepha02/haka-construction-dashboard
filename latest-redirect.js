4f3a95cccefbc07319f935c079048485912bcd87
hepha02 <haka.gpt@gmail.com>
2026-06-30 11:51:27 +0900

    Bump latest version for Excel amount fix

--- latest-redirect.js
+++ latest-redirect.js
@@ -1,4 +1,4 @@
-const LATEST_VERSION = "store-doc-3";
+const LATEST_VERSION = "excel-amount-1";
 const params = new URLSearchParams(location.search);
 const currentVersion = params.get("v") || "";
 const oldVersion = currentVersion && currentVersion !== LATEST_VERSION;
