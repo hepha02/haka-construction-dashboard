f93322728b819ed2c6acbea6404c74c52edc5c8d
hepha02 <haka.gpt@gmail.com>
2026-06-30 11:37:46 +0900

    Bump latest version for auth store doc fix

--- latest-redirect.js
+++ latest-redirect.js
@@ -1,4 +1,4 @@
-const LATEST_VERSION = "store-doc-2";
+const LATEST_VERSION = "store-doc-3";
 const params = new URLSearchParams(location.search);
 const currentVersion = params.get("v") || "";
 const oldVersion = currentVersion && currentVersion !== LATEST_VERSION;
