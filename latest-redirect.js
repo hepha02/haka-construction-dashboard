ef83e0a6c8dc96deb8ac1961e00b1802d22dd170
hepha02 <haka.gpt@gmail.com>
2026-06-30 12:42:12 +0900

    Bump latest version for forced bulk margin

--- latest-redirect.js
+++ latest-redirect.js
@@ -1,4 +1,4 @@
-const LATEST_VERSION = "bulk-margin-1";
+const LATEST_VERSION = "bulk-margin-2";
 const params = new URLSearchParams(location.search);
 const currentVersion = params.get("v") || "";
 const oldVersion = currentVersion && currentVersion !== LATEST_VERSION;
